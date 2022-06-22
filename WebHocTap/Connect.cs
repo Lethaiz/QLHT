ea2ea9d55780b303eb3e5be4ade2e9e6bcd1ed9f
Lethaiz <86224183+Lethaiz@users.noreply.github.com>
2022-06-22 17:18:19 +0700

    Up Admin

---
+++ WebHocTap/Connect.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebHocTap
+
+{
+    public static class Connect
+    {
+        public static IServiceCollection ServicesCollection(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<DbContext>(options => options.UseSqlServer(configuration.GetConnectionString("Context"),
+               x => x.MigrationsAssembly(typeof(DbContext).Assembly.FullName)), ServiceLifetime.Transient);
+
+            return service;
+        }
+    }
+}
